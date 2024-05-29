99610350eb70b51dc7f41c63b6b2e3cc042306cb
JisuLee <jisuejs@gmail.com>
2024-05-30 03:35:35 +0900

    [MODIFY] 주소

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'NanumBarunGothic',
       ),
-      home: SplashScreen(),
+      home: recompage(),
       routes: {
         '/splash': (context) => SplashScreen(),
         '/login': (context) => LoginScreen(),
