edea62f4ae503bc351d97a53fec9d0b8640d194c
CHICHIT <beflylee@naver.com>
2024-06-03 01:25:28 +0900

    플레이리스트 추가

---
+++ lib/playlist/addplaylist.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<String> addSongToPlaylist(int playlistId, int songId) async {
+  final response = await http.get(Uri.parse('http://13.125.27.204:8080/songs-in-playlist?playlist_id=$playlistId&song_id=$songId'));
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> data = jsonDecode(response.body);
+    final bool isSuccess = data['isSuccess'];
+    final String message = data['message'];
+
+    if (isSuccess) {
+      return message;
+    } else {
+      throw Exception('요청에 실패했습니다: $message');
+    }
+  } else {
+    throw Exception('서버 요청 중 오류가 발생했습니다: ${response.statusCode}');
+  }
+}
